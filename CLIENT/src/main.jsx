cb2b4d0526e2c6c83710b8b322debce595f020ac
Emane Daniel Akim <emanedanielakim@gmail.com>
2024-12-01 05:32:12 +0100

    ajout de la police nunito dans le projet et installation de material ui

--- CLIENT/src/main.jsx
+++ CLIENT/src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./api/store.js";
+import "./index.css";
+import "@fontsource/nunito/300.css";
+import "@fontsource/nunito/400.css";
+import "@fontsource/nunito/500.css";
+import "@fontsource/nunito/700.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
